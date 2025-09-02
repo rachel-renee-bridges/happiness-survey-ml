5a765b62d60e4618cb927ee4ce8fc54b3a35736e
rachel-renee-bridges <rachelbridges8@yahoo.com>
2025-09-02 07:31:01 -0500

    Delete src directory

--- src/data/load_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load the ACME Happiness Survey 2020 CSV file from 01_data/01_raw
-def load_acme_data():
-    df = pd.read_csv("01_data/01_raw/ACME-HappinessSurvey2020.csv")
-    return df
-
